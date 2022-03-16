3f3360a2a29e74867bd59f55c0ccfc07cea5fd87
rafa meira <meirarafaela@hotmail.com>
2022-03-15 22:22:06 -0300

    page category

--- src/pages/Frota.js
+++ src/pages/Frota.js
@@ -11,38 +11,42 @@ function Categorias() {
                 <title>DB | Categorias</title>
             </Helmet>
             <main>
-                <section className="bg-black">
+                <section className="titulo-bg">
                     <div className="container">
                         <TitleBgBlack {...title}/>
-                        <div className="carros">
-                            <div className="carros-imagem">
-                                <img src="./img/carros/suv-1.jpg" alt="Modelo Suvs"/>
-                                <span className="font-h4 cor-0">R$ 299.9</span>
-                            </div>
-                            <div className="carros-conteudo">
-                                <h2 className="font-h2">SUVs</h2>
-                                <p className="font-text-1 cor-8">Carros maiores e com um porta-malas mais amplo, para
-                                    quem
-                                    prefere ter mais de espaço, conforto e tecnologia.</p>
-                                <ul className="font-h4 cor-8">
-                                    <li>
-                                        <img src="./img/icones/motor.png" alt=""/>
-                                        Motor Elétrico
-                                    </li>
-                                    <li>
-                                        <img src="./img/icones/carbono.svg" alt=""/>
-                                        Fibra de Carbono
-                                    </li>
-                                    <li>
-                                        <img src="./img/icones/velocidade.svg" alt=""/>
-                                        50 km/h
-                                    </li>
-                                    <li>
-                                        <img src="./img/icones/rastreador.svg" alt=""/>
-                                        Rastreador
-                                    </li>
-                                </ul>
-                                <Link className="botao seta" to="/suvs">Mais Sobre</Link>
+
+                        <div className="carros-white">
+                            <div className="carros">
+                                <div className="carros-imagem">
+                                    <img src="./img/carros/suv-1.jpg" alt="Modelo Suvs"/>
+                                    <span className="font-h4 cor-0">R$ 299.9</span>
+                                </div>
+                                <div className="carros-conteudo">
+                                    <h2 className="font-h2">SUVs</h2>
+                                    <p className="font-text-1 cor-8">Carros maiores e com um porta-malas mais amplo,
+                                        para
+                                        quem
+                                        prefere ter mais de espaço, conforto e tecnologia.</p>
+                                    <ul className="font-h4 cor-8">
+                                        <li>
+                                            <img src="./img/icones/motor.png" alt=""/>
+                                            Motor Elétrico
+                                        </li>
+                                        <li>
+                                            <img src="./img/icones/carbono.svg" alt=""/>
+                                            Fibra de Carbono
+                                        </li>
+                                        <li>
+                                            <img src="./img/icones/velocidade.svg" alt=""/>
+                                            50 km/h
+                                        </li>
+                                        <li>
+                                            <img src="./img/icones/rastreador.svg" alt=""/>
+                                            Rastreador
+                                        </li>
+                                    </ul>
+                                    <Link className="botao seta" to="/suvs">Mais Sobre</Link>
+                                </div>
                             </div>
                         </div>
 
@@ -54,7 +58,8 @@ function Categorias() {
                                 </div>
                                 <div className="carros-conteudo">
                                     <h2 className="font-h2 cor-0">Conversiveis</h2>
-                                    <p className="font-text-1 cor-1">Carros que prezam pelo conforto e luxo, para uma
+                                    <p className="font-text-1 cor-1">Carros que prezam pelo conforto e luxo, para
+                                        uma
                                         experiência na direção incomparável, e sensação de liberdade.</p>
                                     <ul className="font-h4 cor-1">
                                         <li>
@@ -79,38 +84,39 @@ function Categorias() {
                             </div>
                         </div>
 
-                        <div className="carros">
-                            <div className="carros-imagem">
-                                <img src="./img/carros/picape-1.jpg" alt="Modelo Picape"/>
-                                <span className="font-h4 cor-0">R$ 399.9</span>
-                            </div>
-                            <div className="carros-conteudo">
-                                <h2 className="font-h2">Picapes</h2>
-                                <p className="font-text-1 cor-8">Carros ideais para quem está sempre em movimento, e
-                                    curte se
-                                    aventurar. Possui carroceria espaçosa e tração 4x4.</p>
-                                <ul className="font-h4 cor-8">
-                                    <li>
-                                        <img src="./img/icones/motor.png" alt=""/>
-                                        Motor Elétrico
-                                    </li>
-                                    <li>
-                                        <img src="./img/icones/carbono.svg" alt=""/>
-                                        Fibra de Carbono
-                                    </li>
-                                    <li>
-                                        <img src="./img/icones/velocidade.svg" alt=""/>
-                                        50 km/h
-                                    </li>
-                                    <li>
-                                        <img src="./img/icones/rastreador.svg" alt=""/>
-                                        Rastreador
-                                    </li>
-                                </ul>
-                                <Link className="botao seta" to="/picapes">Mais Sobre</Link>
+                        <div className="carros-white">
+                            <div className="carros">
+                                <div className="carros-imagem">
+                                    <img src="./img/carros/picape-1.jpg" alt="Modelo Picape"/>
+                                    <span className="font-h4 cor-0">R$ 399.9</span>
+                                </div>
+                                <div className="carros-conteudo">
+                                    <h2 className="font-h2">Picapes</h2>
+                                    <p className="font-text-1 cor-8">Carros ideais para quem está sempre em movimento, e
+                                        curte se
+                                        aventurar. Possui carroceria espaçosa e tração 4x4.</p>
+                                    <ul className="font-h4 cor-8">
+                                        <li>
+                                            <img src="./img/icones/motor.png" alt=""/>
+                                            Motor Elétrico
+                                        </li>
+                                        <li>
+                                            <img src="./img/icones/carbono.svg" alt=""/>
+                                            Fibra de Carbono
+                                        </li>
+                                        <li>
+                                            <img src="./img/icones/velocidade.svg" alt=""/>
+                                            50 km/h
+                                        </li>
+                                        <li>
+                                            <img src="./img/icones/rastreador.svg" alt=""/>
+                                            Rastreador
+                                        </li>
+                                    </ul>
+                                    <Link className="botao seta" to="/picapes">Mais Sobre</Link>
+                                </div>
                             </div>
                         </div>
-
                         <div className="carros-bg">
                             <div className="carros">
                                 <div className="carros-imagem">
@@ -146,7 +152,6 @@ function Categorias() {
                         </div>
                     </div>
                 </section>
-                <Marca/>
             </main>
         </>
     )
